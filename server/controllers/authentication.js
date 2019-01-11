782c47e84b866104751acb461aa901743de78071
Thiago Scolari <thiagoscolari@MacBook-Pro-de-Thiago.local>
2019-01-11 17:33:59 +0100

    commit

---
+++ server/controllers/authentication.js
@@ -0,0 +1,50 @@
+const jwt = require('jwt-simple')
+const User = require('../models/user')
+const config = require('../config')
+
+function tokenForUser(user){
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next) {
+    //User authenticated and return token here
+    res.send({ token: tokenForUser(req.user)})
+}
+
+exports.signup = function (req, res, next) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //See if a user with a given email exists
+    User.findOne({ email: email }, function (err, existingUser) {
+        if (err) { return next(err) }
+
+        if(!email || !password){
+            return res.status(422).send({ error: 'You must provide an email and password'})
+        }
+        // If a user with an email already exists, return an error
+        if (existingUser) {
+            return res.status(422).send({ error: 'Email is in use' })
+        }
+
+        //If a user with an email does NOT exist, create new user and sabe record
+        const user = new User ({
+            email: email,
+            password: password
+        })
+
+        user.save(function(err){
+            if(err) { return next(err)}
+
+            //Respond to request indicating the user was created
+            res.json({ token: tokenForUser(user)})
+        })
+    })
+
+    //If a user email does exist, return an error
+
+    //If a user with email does NOT exist, create and save user record
+
+    // Respond to request indicating the user was created
+}
