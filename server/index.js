5e2b0d215d798535793c4200834f90979b6c9027
Thiago Scolari <thiagoscolari@MacBook-Pro-de-Thiago.local>
2019-01-11 18:45:43 +0100

    commit

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ mongoose.connect('mongodb://localhost/auth', { useNewUrlParser: true })  //auth 
 
 //App Setup
 app.use(morgan('combined'));
-app.use(cors());
+app.use(cors())
 app.use(bodyParser.json({ type: '*/*'}));
 router(app)
 
