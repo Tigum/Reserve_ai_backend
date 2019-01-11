8b32dec4a9b30bdb52510f1abf39a2fa5a4a0fe4
Thiago Scolari <thiagoscolari@MacBook-Pro-de-Thiago.local>
2019-01-11 18:40:01 +0100

    commit

--- server/index.js
+++ server/index.js
@@ -6,12 +6,14 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 //DB Setup
 mongoose.connect('mongodb://localhost/auth', { useNewUrlParser: true })  //auth is the name of the DB created
 
 //App Setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*'}));
 router(app)
 
