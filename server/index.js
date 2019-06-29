80f7eefc3a0ae31bd5b08ca6fd88660592c51325
Thiago Scolari <thiagoscolari@MacBook-Pro-de-Thiago.local>
2019-06-29 08:01:42 +0100

    commit

--- server/index.js
+++ server/index.js
@@ -7,19 +7,22 @@ const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
 const cors = require('cors')
+const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost/reserve_ai'
 
 //DB Setup
-mongoose.connect('mongodb://localhost/reserve_ai', { useNewUrlParser: true })  //auth is the name of the DB created
+
+mongoose.Promise = global.Promise;
+mongoose.set('degub', true)
+mongoose.connect(CONNECTION_URI, { useNewUrlParser: true })  //auth is the name of the DB created
 
 //App Setup
 app.use(morgan('combined'));
 app.use(cors())
-app.use(bodyParser.json({ type: '*/*'}));
+app.use(bodyParser.json({ type: '*/*' }));
 router(app)
 
 //Server Setup
-const port = process.env.PORT || 3090;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port);
 console.log('Server listening on:', port)
- 
